3653a84b4d73721414bf99952ad4564824fad2fb
nurbaysal <nurbysl__@hotmail.com>
2019-07-17 18:45:32 +0300

    project

---
+++ Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace KullanıcıAdıSifre_HW
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGiris = new System.Windows.Forms.Button();
+            this.txtkullanıcıadı = new System.Windows.Forms.TextBox();
+            this.txtsifre = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnGiris
+            // 
+            this.btnGiris.Location = new System.Drawing.Point(53, 104);
+            this.btnGiris.Name = "btnGiris";
+            this.btnGiris.Size = new System.Drawing.Size(75, 23);
+            this.btnGiris.TabIndex = 0;
+            this.btnGiris.Text = "Giris";
+            this.btnGiris.UseVisualStyleBackColor = true;
+            this.btnGiris.Click += new System.EventHandler(this.BtnGiris_Click);
+            // 
+            // txtkullanıcıadı
+            // 
+            this.txtkullanıcıadı.Location = new System.Drawing.Point(159, 26);
+            this.txtkullanıcıadı.Name = "txtkullanıcıadı";
+            this.txtkullanıcıadı.Size = new System.Drawing.Size(100, 20);
+            this.txtkullanıcıadı.TabIndex = 1;
+            // 
+            // txtsifre
+            // 
+            this.txtsifre.Location = new System.Drawing.Point(159, 64);
+            this.txtsifre.Name = "txtsifre";
+            this.txtsifre.PasswordChar = '*';
+            this.txtsifre.Size = new System.Drawing.Size(100, 20);
+            this.txtsifre.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(40, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Kullanıcı Adı";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(60, 71);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(28, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Sifre";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtsifre);
+            this.Controls.Add(this.txtkullanıcıadı);
+            this.Controls.Add(this.btnGiris);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGiris;
+        private System.Windows.Forms.TextBox txtkullanıcıadı;
+        private System.Windows.Forms.TextBox txtsifre;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KullanıcıAdıSifre_HW
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //İç içe switch kullanarak kullanıcı adını ve şifresini kontrol ediniz.
+            //eğer kullanıcı kullanıcıadını ve şifresini doğru girerse 
+            //"Tebrikler, hem kullanıcı adı hemde şifreniz doğru" uyarısı
+            //Kullanıcı adı doğru şifre yanlışsa
+            //"Kullanıcı adınız doğru ancak şifreniz yanlış!" uyarısı
+            //kullanıcı adı yanlışsa "Kullanıcı adınızı yanlış girdiniz, şifreye bakmadım bile!"
+            //uyarusını kullanıcıya veriniz.
+
+            //KullaniciAdi = "bilgeadam";
+            //Sifre = "1234";
+        }
+
+        private void BtnGiris_Click(object sender, EventArgs e)
+        {
+            switch (txtkullanıcıadı.Text)
+            {
+                case "bilgeadam":
+                    switch (txtsifre.Text)
+                    {
+                        case "1234":
+                            MessageBox.Show("kullanıcı adı ve şifre doğru");
+                            break;
+                        default:
+                            MessageBox.Show("kulanıcı adı doğru ancak şifre yanlış");
+                            break;
+                    }
+                    break;
+                default:
+                    MessageBox.Show("kullanıcı adı yanlış şifreye bakmadım bile");
+                    break;
+            }
+        }
+    }
+}
